7b3882c99370093ca868b96f090c1c250ceda492
Daniel P. M <danielmoronta23@gmail.com>
2020-06-23 12:42:19 -0400

    Finalized

--- src/main/java/practica2/controladores/ControladorPlantilla.java
+++ src/main/java/practica2/controladores/ControladorPlantilla.java
@@ -3,12 +3,13 @@ package practica2.controladores;
 import io.javalin.Javalin;
 import io.javalin.plugin.rendering.JavalinRenderer;
 import io.javalin.plugin.rendering.template.JavalinFreemarker;
+import io.javalin.plugin.rendering.template.JavalinThymeleaf;
 import practica2.util.ControladorBase;
 
 public class ControladorPlantilla extends ControladorBase {
     public ControladorPlantilla(Javalin app) {
         super(app);
-        JavalinRenderer.register(JavalinFreemarker.INSTANCE, ".ftl");
+        JavalinRenderer.register(JavalinThymeleaf.INSTANCE, ".html");
     }
 
     @Override
